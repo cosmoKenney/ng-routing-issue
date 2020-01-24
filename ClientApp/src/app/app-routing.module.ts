5681b4490c88d2302387e253a565162d2f3e7412
Ken Hadden <khadden@provpubs.com>
2020-01-23 17:25:46 -0800

    Add angular and test with ng serve

---
+++ ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from "./test/test.component";
+
+
+const routes: Routes = [
+	{
+		path: 'search',
+		component: TestComponent
+	}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
